50ef4b32d6c837e73be5663d5e20670594c0fbd7
Bryan Kendall <bryan.a.kendall@gmail.com>
2014-08-07 14:21:08 -0700

    env port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, FON\n');
-}).listen(80);
-console.log('Server running at http://127.0.0.1:80/');
+}).listen(port);
+console.log('Server running at http://127.0.0.1:' + port + '/');
