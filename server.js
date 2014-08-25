619b2e11b6d655f8cbcb588100998f4e53d88afa
Bryan Kendall <bryan.a.kendall@gmail.com>
2014-08-25 13:25:40 -0700

    adding fon_user

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var port = process.env.PORT || 8080;
+var user = process.env.FON_USER || 'user';
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, FON\n\nServed on port ' + port + '\n');
+  res.end('Hello, ' + user + '!\n\nServed on port ' + port + '\n');
 }).listen(port);
 console.log('Server running at http://127.0.0.1:' + port + '/');
