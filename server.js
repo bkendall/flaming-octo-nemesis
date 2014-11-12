94c91871a242a29507ded45c414b9752edce4e64
Bryan Kendall <bryan.a.kendall@gmail.com>
2014-11-12 09:23:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var port = process.env.PORT || 8080;
 var user = process.env.FON_USER || 'user';
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, ' + user + '!\n\nServe-ed on port ' + port + '\n');
+  res.end('Hello, ' + user + '!\n\nServed on port ' + port + '\n');
 }).listen(port);
 console.log('Server running at http://127.0.0.1:' + port + '/');
