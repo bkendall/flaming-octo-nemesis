34a728c59e713b7fbf5b0d6ed3a8e4f4e2c695c5
Bryan Kendall <bryan.a.kendall@gmail.com>
2015-02-09 17:04:02 -0800

    comma

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, ' + user + '!\n\nServed on port ' + port + '\n');
 }).listen(port);
-console.log('Server running at http://127.0.0.1:' + port + '/');
+console.log('Server running, at http://127.0.0.1:' + port + '/');
